9093411ce807e9767b42b81a0a3ed892260ed78f
Maik Schneider <mas@xima.de>
2022-05-19 15:28:59 +0200

    feat: add npm build command

--- webpack.js
+++ webpack.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 module.exports = {
 	mode: 'development',
-	devtool: 'inline-source-map',
-	watch: true,
 	entry: {
 		MailCatcher: './Resources/Private/TypeScript/MailCatcher.ts'
 	},
