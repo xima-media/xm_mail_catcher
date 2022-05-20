b0fec2fe38d8efe0166087b55cafa55e94885816
Maik Schneider <mas@xima.de>
2022-05-20 09:08:05 +0200

    fix: add missing typescript files

---
+++ typo3_src/TypeScript/core/Resources/Public/TypeScript/Ajax/SimpleResponseInterface.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck
+export default interface SimpleResponseInterface {
+  status: number;
+  headers: Map<string, string>;
+  body: string | any;
+}
---
+++ typo3_src/TypeScript/core/Resources/Public/TypeScript/lit-helper.ts
@@ -0,0 +1,43 @@
+// @ts-nocheck
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+import {render, TemplateResult} from 'lit/html';
+
+/**
+ * @internal
+ */
+export const renderNodes = (result: TemplateResult): NodeList => {
+  const anvil = document.createElement('div');
+  render(result, anvil);
+  return anvil.childNodes;
+};
+
+/**
+ * @internal
+ */
+export const renderHTML = (result: TemplateResult): string => {
+  const anvil = document.createElement('div');
+  render(result, anvil);
+  return anvil.innerHTML;
+}
+
+/**
+ * @internal
+ */
+export const lll = (key: string): string => {
+  if (!window.TYPO3 || !window.TYPO3.lang || typeof window.TYPO3.lang[key] !== 'string') {
+    return '';
+  }
+  return window.TYPO3.lang[key];
+};
