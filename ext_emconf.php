7da84515c2151f7a8de82c26949a31a574bf2aed
Maik Schneider <mas@xima.de>
2022-05-23 14:00:14 +0200

    feat: change typo version

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.99.99',
+            'typo3' => '10.0.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
